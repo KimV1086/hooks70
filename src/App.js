9fb3c6b29dd5110ca075995a8f59198f4e010b10
Vitalik Kim <kimv1086@gmail.com>
2020-01-18 14:34:14 +0600

    added axios

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import InputForm from "./components/InputForm/InputForm";
 import './App.css';
+import Container from "reactstrap/lib/Container";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Container>
+        <InputForm/>
+    </Container>
   );
 }
 
